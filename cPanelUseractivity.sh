5141aaab8ce0f754d7808baea4dd656461533f14
harish-nischal <145145000+harish-nischal@users.noreply.github.com>
2025-06-29 13:40:41 -0400

    Create cPanelUseractivity.sh
    Created new file

---
+++ cPanelUseractivity.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+## Author: Optimized by ChatGPT (original: Michael Ramsey)
+## https://github.com/harish-nischal/scriptforce
+## Objective: Find cPanel user's login records and activities with date filtering and color output
+##
+## How to use:
+## ./cPanelLoginLogsByUser.sh <username>
+## Example:
+## ./cPanelLoginLogsByUser.sh exampleuserbob
